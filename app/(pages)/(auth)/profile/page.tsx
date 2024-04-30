41a75b76667595cbb05d073c94fd946b00eb800e
atalaykarahan <krhnatalay@gmail.com>
2024-04-30 21:05:42 +0300

    temel düzeyde folder structher ayarlandı henüz shadcn ve tailwind yok

---
+++ app/(pages)/(auth)/profile/page.tsx
@@ -0,0 +1,7 @@
+const ProfilePage = () => {
+    return ( <div>
+        <h1>This is for if user wants a change profile settings then he can see it</h1>
+    </div> );
+}
+ 
+export default ProfilePage;
---
+++ app/(pages)/home/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <p>this screen for when user successful login he can see it</p>
+    </div>
+  );
+};
+
+export default HomePage;

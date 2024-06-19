50002fc015af3c9ea01f6e82f8ad147d37b3cd76
atalaykarahan <krhnatalay@gmail.com>
2024-06-19 17:49:04 +0300

    bilinmeyen bir hata kartı component haline getirildi

--- app/(pages)/(auth)/createname/page.tsx
+++ app/(pages)/(auth)/createname/page.tsx
@@ -1,31 +1,18 @@
 "use client";
 import CustomCard from "@/components/custom-card";
+import UnknownErrorCard from "@/components/unknown-error-card";
+import { useSearchParams } from "next/navigation";
 import CreateNameForm from "./create-name-form";
 import GoogleSection from "./google-section";
 import TitleSection from "./title-section";
-import { useSearchParams } from "next/navigation";
-import Image from "next/image";
 
 const CreateName = () => {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
 
-  if (token == null)
-    return (
-      <CustomCard className="max-w-sm w-full mx-auto rounded-none text-center md:rounded-2xl p-4 md:p-8 relative">
-        <Image 
-        src={"error.png"} 
-        alt={""}
-        width={50}
-        height={50}
-        className="w-[200px] mx-auto"
-        />
-        <p className="text-white text-2xl pt-10 font-medium">Bilinmeyen bir hata ile karşılaşıldı.</p>
-      </CustomCard>
-    );
+  if (token == null) return <UnknownErrorCard />;
 
   return (
-
     <CustomCard className="max-w-md  w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 relative">
       {/* baslik yazilarin bulundugu kisim */}
       <TitleSection />
---
+++ components/unknown-error-card.tsx
@@ -0,0 +1,21 @@
+import CustomCard from "./custom-card";
+import Image from "next/image";
+
+const UnknownErrorCard = () => {
+  return (
+    <CustomCard className="max-w-sm w-full mx-auto rounded-none text-center md:rounded-2xl p-4 md:p-8 relative">
+      <Image
+        src={"error.png"}
+        alt={""}
+        width={50}
+        height={50}
+        className="w-[200px] mx-auto"
+      />
+      <p className="text-white text-2xl pt-10 font-medium">
+        Bilinmeyen bir hata ile karşılaşıldı.
+      </p>
+    </CustomCard>
+  );
+};
+
+export default UnknownErrorCard;

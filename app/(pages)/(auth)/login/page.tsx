f022a3fe4896a260a1395ebbf955e714a96eea70
kwa0x2 <alperenibo14@gmail.com>
2024-06-19 21:02:37 +0300

    logindeki loading ve hata ayarlandi

--- app/(pages)/(auth)/login/page.tsx
+++ app/(pages)/(auth)/login/page.tsx
@@ -2,34 +2,46 @@
 
 import { loginAction } from "@/actions/login";
 import { getLoggedInUser } from "@/app/api/services/auth.Service";
-import { useEffect } from "react";
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import Loading from "@/public/blocks-wave.svg";
+import Image from "next/image";
+import UnknownErrorCard from "@/components/unknown-error-card";
 
 const LoginPage = () => {
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
-      const res: any = await getLoggedInUser();
+      try {
+        const res: any = await getLoggedInUser();
 
-      if (res.error) console.log("burda hata döndür");
+        if (res.error) setError(true);
 
-      //kullanici basarili bir sekilde giris yapmis demektir
-      if (res) {
-        console.log(res);
-        loginAction(
-          res.data.id,
-          res.data.name,
-          res.data.mail,
-          res.data.photo
-        );
+        //kullanici basarili bir sekilde giris yapmis demektir
+        if (res) {
+          console.log(res);
+          loginAction(
+            res.data.id,
+            res.data.name,
+            res.data.mail,
+            res.data.photo
+          );
+        }
+      } catch (error: any) {
+        setError(true);
+        console.error(error);
       }
     };
 
     fetchData();
   }, []);
 
+  if (error) return <UnknownErrorCard />;
+
   return (
-    <div className="bg-white">
-      alper buraya güzel bir yükleme veya dönen circle bul şöyle bir şey yazsın
-      ekranda &apos;yönlendiriliyor&apos;
+    <div className="w-screen h-screen items-center flex justify-center backdrop-blur-sm ">
+      <Image src="/loading.svg" alt="Loading" width={50} height={50} />
     </div>
   );
 };

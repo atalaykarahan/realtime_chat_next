73a52f85c1f27d4359a4621a428f175854f918fd
atalaykarahan <krhnatalay@gmail.com>
2024-06-27 22:15:55 +0300

    chat kısmı componentlere ayrıldı ve back-end tarafından mesaj getirme sağlandı

--- app/(pages)/chat/page.tsx
+++ app/(pages)/chat/page.tsx
@@ -1,13 +1,14 @@
 "use client";
+import ConnectionStatus from "@/components/connection-status";
 import CustomCard from "@/components/custom-card";
-import DummyData from "@/components/ui/dummy";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { useEffect, useState } from "react";
+import io, { Socket } from "socket.io-client";
 import ChatMessage from "./chat-message";
 import ChatNavbar from "./chat-navbar";
-import UserProfile from "./user-profile/user-profile";
-import { useCurrentUser } from "@/hooks/use-current-user";
-import io, { Socket } from "socket.io-client";
-import { useEffect, useState } from "react";
-import ConnectionStatus from "@/components/connection-status";
+
+import Sidebar from "./sidebar/sidebar";
+import Speech from "./speech/speech";
 // import { auth } from "@/auth";
 
 const ChatPage = () => {
@@ -15,6 +16,7 @@ const ChatPage = () => {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [connectionStatus, setConnectionStatus] =
     useState<string>("Bağlanıyor...");
+  const [message, setMessage] = useState<string>("");
 
   useEffect(() => {
     const newSocket = io("http://localhost:9898/chat", {
@@ -29,6 +31,11 @@ const ChatPage = () => {
     newSocket.on("connect_error", (error) => {
       setConnectionStatus(`Bağlantı Hatası: ${error.message}`);
     });
+
+    newSocket.on("merhaba", (data) => {
+      setMessage(data);
+    });
+
     setSocket(newSocket);
     return () => {
       if (newSocket) newSocket.close();
@@ -42,25 +49,15 @@ const ChatPage = () => {
         className="h-screen w-screen p-6 flex gap-5 relative"
         style={{ zIndex: "1" }}
       >
+        <div className="bg-white"> {message}</div>
         {/* {JSON.stringify(session)} */}
-        <CustomCard className="hidden lg:block flex-none min-w-[260px]">
-          <UserProfile user={user} />
-          <div className="pt-3">
-            <DummyData />
-            <DummyData />
-            <DummyData />
-            <DummyData />
-            <DummyData />
-            <DummyData />
-            <DummyData />
-            <DummyData />
-            <DummyData />
-            <DummyData />
-            <DummyData />
-          </div>
-        </CustomCard>
+        <Sidebar user={user} />
+
         <CustomCard className="flex-1 flex flex-col justify-between">
+          {/* chatin ust kisminda konusulan kisinin resminin falan oldugu yer */}
           <ChatNavbar />
+          <Speech user={user} />
+
           <div className="mt-auto">
             <ChatMessage />
           </div>

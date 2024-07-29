2201697c8d7441c6ed4d5b65aad75bf20c7e7bef
atalaykarahan <krhnatalay@gmail.com>
2024-07-29 23:01:14 +0300

    bg açıldı ve bazı kullanılmayan kodlar yorum satırı yapıldı

---
+++ app/api/services/message.Service.ts
@@ -0,0 +1,16 @@
+import axios from "../axios";
+
+// export const PostPrivateConversation = async (
+//     sender_id: string,
+//     receiver_id: string
+// ) => {
+//     const body = {
+//         message_sender_id: sender_id,
+//         message_receiver_id: receiver_id,
+//     };
+//     return await axios.post("/message/conversation/private", body);
+// };
+
+// export const GetConversation = async () => {
+//   return await axios.get("/message/conversation");
+// };
